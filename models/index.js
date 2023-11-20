09da641be72d4c3054b4278c07112545a85a7f28
ZBurnell <136769106+ZBurnell@users.noreply.github.com>
2023-11-20 14:35:12 -0500

    Updated all of the model files

--- models/index.js
+++ models/index.js
@@ -5,12 +5,24 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: '',
+  onDelete:'CASCADE',
+});
 
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: '',
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  foreignKey: '',
+  onDelete: 'CASCADE',
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  foreignKey: '',
+});
 
 module.exports = {
   Product,
